d191b5dca12ae06a454e4f23af2127d0b8b27fff
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-04-07 15:16:36 +0700

    add method save and load

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+public static class DataManager
+{
+    
+    public static void SaveData (string name_, int score_, string GAMEMODE) {
+        string path;
+
+        DateTime timestamp = DateTime.Now;
+        string unixTime = ((DateTimeOffset)timestamp).ToUnixTimeSeconds().ToString();
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        if (GAMEMODE == "ZEN") {
+            path = Application.persistentDataPath + "/zen/" + unixTime + ".fun";
+        } else {
+            path = Application.persistentDataPath + "/waves/" + unixTime + ".fun";
+        }
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(name_, score_);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer(string GAMEMODE) {
+        string path;
+
+        DateTime timestamp = DateTime.Now;
+        string unixTime = ((DateTimeOffset)timestamp).ToUnixTimeSeconds().ToString();
+
+        if (GAMEMODE == "ZEN") {
+            path = Application.persistentDataPath + "/zen/" + unixTime + ".fun";
+        } else {
+            path = Application.persistentDataPath + "/waves/" + unixTime + ".fun";
+        }
+
+        if (File.Exists(path)) {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        } else {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public string name;
+    public int score;
+
+    public PlayerData (string name_, int score_) {
+        name = name_;
+        score = score_;
+    }
+
+}
