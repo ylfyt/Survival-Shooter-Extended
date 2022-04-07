740af16153d336172c73a2d8ceb9dee257c58c04
13519001 Karlsen Adiyasa Bachtiar <13519001@std.stei.itb.ac.id>
2022-04-07 23:46:38 +0700

    add waves mode

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -6,12 +6,38 @@ public class EnemyManager : MonoBehaviour
     // public GameObject enemy;
     public float spawnTime = 3f;
 
+    public int totalEnemies;
+    public static int remainingEnemies;
+    public static int level;
+    public bool isZenMode = true;
+
     [SerializeField]
     MonoBehaviour factory;
     IFactory Factory { get { return factory as IFactory; } }
     void Start()
     {
-        InvokeRepeating("Spawn", spawnTime, spawnTime);
+        if (isZenMode)
+        {
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
+        } else {
+            level = 1;
+            SpawnWave();
+            level++;
+        }
+        
+    }
+
+    void Update() {
+        if (playerHealth.currentHealth <= 0f) { return; }
+
+        if (!isZenMode)
+        {
+            if (remainingEnemies <= 0 )
+            {
+                SpawnWave();
+                level++;
+            }
+        }
     }
 
 
@@ -24,6 +50,19 @@ public class EnemyManager : MonoBehaviour
 
         int spawnEnemy = Random.Range(0, 3);
         Factory.FactoryMethod(spawnEnemy);
+        Debug.Log("WAVES remaining enemies : " + totalEnemies);
 
+    }
+
+    void SpawnWave()
+    {
+        
+        totalEnemies = 10 * level;
+        remainingEnemies = totalEnemies;
+        while (totalEnemies > 0)
+        {
+            Spawn();
+            totalEnemies--;
+        }
     }
 }
