125603200e898f9cad89fa28b9342c53357d26cc
yudialfayat <ayudi3521@gmail.com>
2022-04-09 01:36:31 +0700

    GameOver Scene

--- Assets/Scripts/Managers/ShowGameOver.cs
+++ Assets/Scripts/Managers/ShowGameOver.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ShowGameOver : MonoBehaviour
 {
+    public Text nameText;
+    public Text modeText;
+    public Text scoreText;
+    public Text levelText;
     string username = "";
     string mode = "zen";
     int score = 0;
@@ -15,9 +21,29 @@ public class ShowGameOver : MonoBehaviour
         score = PlayerPrefs.GetInt("score", 0);
         waveLevel = PlayerPrefs.GetInt("wave-level", 0);
 
-        Debug.Log(username);
-        Debug.Log(mode);
-        Debug.Log(score);
-        Debug.Log(waveLevel);
+        nameText.text = username;
+        modeText.text = mode.ToUpper() + " Mode";
+
+        if (mode == "zen")
+        {
+            scoreText.text = "Score: " + score.ToString() + " s";
+            levelText.enabled = false;
+            return;
+        }
+
+        scoreText.text = "Score: " + score.ToString();
+        levelText.enabled = true;
+        levelText.text = waveLevel.ToString();
+    }
+
+    public void GoToReplay()
+    {
+        PlayerPrefs.SetString("mode", mode);
+        SceneManager.LoadScene(sceneName: "Level_01");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "Menu");
     }
 }
