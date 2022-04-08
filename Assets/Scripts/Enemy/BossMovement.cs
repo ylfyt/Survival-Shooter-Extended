4df1191b30e741a3df1996c742846b7b42958fcc
yudialfayat <ayudi3521@gmail.com>
2022-04-08 10:58:02 +0700

    Boss Movement

---
+++ Assets/Scripts/Enemy/BossMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+    Animator anim;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (enemyHealth == null)
+        {
+            Debug.Log("EnemyHealt is null");
+        }
+        if (playerHealth == null)
+        {
+            Debug.Log("PlayerHealth is null");
+        }
+        if (enemyHealth.currentHealth > 0 && !playerHealth.isDead)
+        {
+            nav.SetDestination(player.position);
+            if (nav.remainingDistance <= nav.stoppingDistance)
+            {
+                anim.SetBool("Walk", false);
+            }
+            else
+            {
+                anim.SetBool("Walk", true);
+            }
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
