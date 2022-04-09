07c1afbc140c9f8b8c79cc541d710866541fb8b8
almeizaarvin <13519066@std.stei.itb.ac.id>
2022-04-09 16:50:02 +0700

    Add randomization for orb type

--- Assets/OrbManager.cs
+++ Assets/OrbManager.cs
@@ -10,6 +10,7 @@ public class OrbManager : MonoBehaviour
     public GameObject HealthOrb;
     public int xPos;
     public int zPos;
+    public int orbType;
 
     void Start()
     {
@@ -20,8 +21,20 @@ public class OrbManager : MonoBehaviour
         while(true){
             xPos = Random.Range(-10, -24);
             zPos = Random.Range(-10, -24);
-            Instantiate(PowerOrb, new Vector3(xPos, 0.6f, zPos), Quaternion.identity);
-            yield return new WaitForSeconds(5f);
+            orbType = Random.Range(1, 3);
+            switch(orbType){
+                case 1:
+                    Instantiate(PowerOrb, new Vector3(xPos, 0.6f, zPos), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(SpeedOrb, new Vector3(xPos, 0.6f, zPos), Quaternion.identity);                    
+                    break;
+                default:
+                    Instantiate(HealthOrb, new Vector3(xPos, 0.6f, zPos), Quaternion.identity);                    
+                    break;
+            }
+            
+            yield return new WaitForSeconds(10f);
         }
        
     }
