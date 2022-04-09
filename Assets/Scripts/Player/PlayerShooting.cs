6deda8939e4598837b8b4cc8d1b4e57dfcb30b85
almeizaarvin <13519066@std.stei.itb.ac.id>
2022-04-10 02:38:45 +0700

    Fix gun direction

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -12,6 +12,12 @@ public class PlayerShooting : MonoBehaviour
 
     float timer;
     Ray shootRay;
+    Ray shootRay2;
+    Ray shootRay3;
+    Ray shootRay4;
+    Ray shootRay5;
+    public List<Ray> shootRayList = new List<Ray>();
+
     RaycastHit shootHit;
     int shootableMask;
     ParticleSystem gunParticles;
@@ -20,10 +26,10 @@ public class PlayerShooting : MonoBehaviour
     public LineRenderer gunLine3;
     public LineRenderer gunLine4;
     public LineRenderer gunLine5;
+    public List<LineRenderer> gunLineList = new List<LineRenderer>();
 
 
     public int gunDirectionLevel = 1;
-    public List<LineRenderer> gunLineList = new List<LineRenderer>();
 
     AudioSource gunAudio;
     Light gunLight;
@@ -66,11 +72,20 @@ public class PlayerShooting : MonoBehaviour
     {
         line.SetPosition(0, gunPoint.position);
         line.SetPosition(1, gunPoint.position + Quaternion.Euler(0, degree, 0) * gunPoint.forward * range);
+        gunLineList.Add(line);
+    }
+
+    public void setShootRay(Ray shootRay, int degree){
+        shootRay.origin = gunPoint.position;
+        shootRay.direction = Quaternion.Euler(0, degree, 0) * gunPoint.forward;
+        shootRayList.Add(shootRay);
     }
 
     void Shoot()
     {
         timer = 0f;
+        gunLineList.Clear();
+        shootRayList.Clear();
 
         gunAudio.Play();
         gunLight.enabled = true;
@@ -78,60 +93,72 @@ public class PlayerShooting : MonoBehaviour
         gunParticles.Play();
 
         gunLine.enabled = true;
-        gunLine.SetPosition(0, gunPoint.position);
-        gunLineList.Add(gunLine);
+        setDirection(gunLine, 0);
+        setShootRay(shootRay, 0);
+
 
         if (gunDirectionLevel == 2)
         {
             gunLine2.enabled = true;
             setDirection(gunLine2, -23);
+            setShootRay(shootRay2 ,-23);
 
             gunLine3.enabled = true;
             setDirection(gunLine3, 23);
+            setShootRay(shootRay3 ,23);
 
-            gunLineList.Add(gunLine2);
-            gunLineList.Add(gunLine3);
         }
 
         else if (gunDirectionLevel == 3)
         {
+     
             gunLine2.enabled = true;
             setDirection(gunLine2, -23);
+            setShootRay(shootRay2 ,-23);
 
             gunLine3.enabled = true;
             setDirection(gunLine3, 23);
+            setShootRay(shootRay3 ,23);
 
             gunLine4.enabled = true;
             setDirection(gunLine4, -45);
+            setShootRay(shootRay4, -45);
+
 
             gunLine5.enabled = true;
             setDirection(gunLine5, 45);
+            setShootRay(shootRay5, 45);
 
-
-            gunLineList.Add(gunLine4);
-            gunLineList.Add(gunLine5);
         }
 
 
-        shootRay.origin = gunPoint.position;
-        shootRay.direction = gunPoint.forward;
-
-        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
-        {
-            EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
-
-            if (enemyHealth != null)
+       for (int i = 0; i < shootRayList.Count; i++) {
+          var SR = shootRayList[i];
+          if (Physics.Raycast(SR, out shootHit, range, shootableMask))
             {
-                enemyHealth.TakeDamage(playerAttribute.power * damagePerShot, shootHit.point);
-            }
+                EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
 
-            gunLine.SetPosition(1, shootHit.point);
-            var hitEffect = Instantiate(hitParticle, shootHit.point, Quaternion.LookRotation(shootHit.normal));
-            Destroy(hitEffect, .3f);
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(playerAttribute.power * damagePerShot, shootHit.point);
+                }
+
+                
+                gunLineList[i].SetPosition(1, shootHit.point);
+                var hitEffect = Instantiate(hitParticle, shootHit.point, Quaternion.LookRotation(shootHit.normal));
+                Destroy(hitEffect, .3f);
+            
+                // gunLine.SetPosition(1, shootHit.point);
+                // var hitEffect = Instantiate(hitParticle, shootHit.point, Quaternion.LookRotation(shootHit.normal));
+                // Destroy(hitEffect, .3f);
+            }
+            else
+            {
+                gunLineList[i].SetPosition(1, SR.origin + SR.direction * range);
+            }
+        
         }
-        else
-        {
-            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
-        }
-    }
+           
+    }   
+    
 }
